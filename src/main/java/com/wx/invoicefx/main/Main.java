b0e880129cf3b455d587dd39639ce499a90ba029
Canale <raffaele.canale@epfl.ch>
2017-05-06 10:21:12 +0200

    Reset

---
+++ src/main/java/com/wx/invoicefx/main/Main.java
@@ -0,0 +1,13 @@
+package com.wx.invoicefx.main;
+
+/**
+ * @author Raffaele Canale (<a href="mailto:raffaelecanale@gmail.com?subject=InvoiceFX">raffaelecanale@gmail.com</a>)
+ * @version 0.1 - created on 05.05.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
